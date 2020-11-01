be9619ad1577d3741bbecf2b1ebf921068bf9f07
“drosso“ <drosso@mpf.gov.ar>
2020-10-31 21:00:34 -0300

    validaciones

--- ClientApp/src/app/productos/productos-form/productos-form.component.ts
+++ ClientApp/src/app/productos/productos-form/productos-form.component.ts
@@ -31,7 +31,6 @@ export class ProductosFormComponent implements OnInit {
                     if( isNaN(this.productoId)  )
                     {
                       //return;
-
                     }
                     else{
                       this.modoEdicion=true;
